1c376bb825ada5cc3c6a331d397565f1aae47bd0
Ruslan Kibets <ruslan.kibets@gmail.com>
2021-04-24 13:52:31 -0700

    Exited once

--- Assets/Scripts/GameLoop/ExitTrigger.cs
+++ Assets/Scripts/GameLoop/ExitTrigger.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class ExitTrigger : MonoBehaviour
 {
+    private bool _exitedOnce; 
+    
     private void OnTriggerEnter(Collider other)
     {
+        if (_exitedOnce) return;
+        
         if (other.CompareTag("Player"))
         {
+            _exitedOnce = true;
+            
             FindObjectOfType<GameLoopController>().OnExit();
         }
     }
