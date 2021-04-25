39c5e51967b9ef1835b17feeaf75841450b3d8c3
Alexey Grishin <alexey.grishin@gmail.com>
2021-04-25 12:15:15 +0300

    двер анимироват ключи чинит

--- Assets/Scripts/Environment/Key.cs
+++ Assets/Scripts/Environment/Key.cs
@@ -8,12 +8,19 @@ public class Key : MonoBehaviour
     private Flameable _flameable;
 
     public UnityEvent<bool> OnKeyFire;
+    public UnityEvent<float> OnKeyFireProgress;
     
     private void Start()
     {
         _flameable = GetComponentInChildren<Flameable>();
         _flameable.OnFlameStopped += FlameableOnOnFlameStopped;
         _flameable.OnFlameStarted += FlameableOnOnFlameStarted;
+    }
+
+    private void Update()
+    {
+        if (_flameable.IsBurning)
+            OnKeyFireProgress.Invoke(_flameable.Progress);
     }
 
     private void FlameableOnOnFlameStarted()
