95fc3874c58ae0f4d748d0750c5e11fb393ef815
Alexey Grishin <alexey.grishin@gmail.com>
2021-04-25 18:13:49 +0300

    разумные торшеры разумны нефть хернефть

---
+++ Assets/Scripts/Dekor/Afraid.cs
@@ -0,0 +1,99 @@
+﻿
+using System;
+using DG.Tweening;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Afraid : MonoBehaviour
+{
+    private TweenIdle _idle;
+
+    public float Angle = 0;
+    public float Scale = 1;
+    public Vector3 PositionShift = Vector3.zero;
+
+    public float Duration = 0.3f;
+    private Vector3 _initialPosition;
+
+    private void Start()
+    {
+        _idle = GetComponent<TweenIdle>();
+        _initialPosition = transform.position;
+    }
+
+    public void SetAfraid()
+    {
+        if (_idle)
+        {
+            _idle.enabled = false;
+            DOTween.Kill(_idle.transform);
+        }
+        
+        if (Angle != 0)
+        {
+            transform.DOLocalRotateQuaternion(quaternion.Euler(0, 0, Angle), Duration);
+        }
+
+        if (Scale != 1)
+        {
+            transform.DOScale(Scale, Duration);
+        }
+
+        if (PositionShift != Vector3.zero)
+        {
+            transform.DOMove(_initialPosition + PositionShift, Duration);
+        }
+    }
+
+    public void SetUnafraid()
+    {
+        if (Angle != 0)
+        {
+            transform.DOLocalRotateQuaternion(Quaternion.Euler(0, 0, 0), Duration).onComplete += () =>
+            {
+                if (_idle) _idle.enabled = true;
+            };
+        }
+
+        if (Scale != 1)
+        {
+            transform.DOScale(1, Duration).onComplete += () =>
+            {
+                if (_idle) _idle.enabled = true;
+            };
+        }
+
+        if (PositionShift != Vector3.zero)
+        {
+            transform.DOMove(_initialPosition, Duration).onComplete += () =>
+            {
+                if (_idle) _idle.enabled = true;
+            };
+        }
+        
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (var afraid in GetComponentsInChildren<Afraid>())
+            {
+                afraid.SetAfraid();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach (var afraid in GetComponentsInChildren<Afraid>())
+            {
+                afraid.SetUnafraid();
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Dekor/TweenIdle.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using DG.Tweening;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TweenIdle : MonoBehaviour
+{
+
+    public float EverySeconds = 1;
+    public float TweenAngleAtRandom = 0;
+
+    public float TweenScaleAtRandom = 0;
+    
+    public float Duration = 0.5f;
+    public Ease Ease;
+    
+    private void Start()
+    {
+        InvokeRepeating(nameof(DoTween), EverySeconds, EverySeconds);
+    }
+
+    void DoTween()
+    {
+        if (!enabled) return;
+        var newAngle = Random.Range(-TweenAngleAtRandom, TweenAngleAtRandom);
+        transform.DOLocalRotateQuaternion(Quaternion.Euler(0, 0, newAngle), Duration).SetEase(Ease);
+        var newScale = 1 + Random.Range(-TweenScaleAtRandom, TweenScaleAtRandom);
+        transform.DOScale(newScale, Duration).SetEase(Ease);
+    }
+}
