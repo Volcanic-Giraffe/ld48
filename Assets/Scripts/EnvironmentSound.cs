1723f11ae5be8b61a4b7c7ba2e397a6a3de54402
A333 <arz.giyan@gmail.com>
2021-04-26 03:41:04 +0300

    Update whisper loop

--- Assets/Scripts/EnvironmentSound.cs
+++ Assets/Scripts/EnvironmentSound.cs
@@ -5,10 +5,12 @@ public class EnvironmentSound : MonoBehaviour
     public float radius;
 
     public string loopSound;
+    public string randomSound;
+    public float Cooldown = 10f; 
 
     private HeroScript _hero;
     private Sounds _sounds;
-
+    private float cooldown = 0;
     private void Awake()
     {
         _sounds = FindObjectOfType<Sounds>();
@@ -24,22 +26,33 @@ public class EnvironmentSound : MonoBehaviour
         {
             if (Vector3.Distance(transform.position, _hero.transform.position) < radius)
             {
-                _sounds.PlayLoop(loopSound);
+                if (!string.IsNullOrEmpty(loopSound))
+                    _sounds.PlayLoop(loopSound);
+                else if (!string.IsNullOrEmpty(randomSound))
+                {
+                    cooldown -= Time.deltaTime;
+                    if (cooldown < 0)
+                    {
+                        cooldown = Cooldown;
+                        _sounds.PlayRandom(randomSound);
+                    }
+                }
             }
             else
             {
-                _sounds.StopLoop(loopSound);
+                if (!string.IsNullOrEmpty(loopSound))
+                    _sounds.StopLoop(loopSound);
             }
         }
     }
 
 #if UNITY_EDITOR
-    
+
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, radius);
     }
-    
+
 #endif
 }
